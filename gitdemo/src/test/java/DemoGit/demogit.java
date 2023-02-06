081999a56bef6e9960a0fb25b2229c76bbbd8ee9
ritujain1208 <ritu@singleinterface.com>
2023-02-06 11:11:33 +0530

    add git demo

---
+++ gitdemo/src/test/java/DemoGit/demogit.java
@@ -0,0 +1,11 @@
+package DemoGit;
+
+import org.testng.annotations.Test;
+
+public class demogit {
+
+	@Test
+	public static void tsetup() {
+		System.out.println("this is gti demo Project **************************");
+	}
+}
